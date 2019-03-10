3cb0095cf57932c8f73a85558df5db329b34f219
deanreidy <deanreidy24@gmail.com>
2019-03-10 22:36:08 +0000

    App with my main fitness page as front page, when clicking add fitness class brings me back to main page.

---
+++ app/src/main/java/com/example/myfitnessapp/activities/MainAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.myfitnessapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainAdapter : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_adapter)
+    }
+}
